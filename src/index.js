871560d1d654b1cbc20a6464786f92fe9fe4296f
evanina <eva.degano@gmail.com>
2022-01-20 19:42:33 +0100

    iteration 0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
