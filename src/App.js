832c1e7ffca46afaf577d793f3e942182cbf8701
Cindy Confiant <cindy.confiant@gmail.com>
2022-01-21 16:00:13 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import './App.css';
-import countries from './countries.json';
+import jsonCountries from './countries.json';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 
 class App extends React.Component {
+  state = {
+    countries : []
+  }
+
+  componentDidMount() {
+    console.log('mount : update the App state from [] to [countries from json]');
+    this.setState({
+      countries: jsonCountries
+    })
+  }
 
   render() {
-
     return (
       <div className="App">
         <Navbar />
         
         <div className='container'>
           <div className="row">
-            <CountriesList countries={countries}/>
-
-            <Route path='/:cca3' component={CountryDetails} />
+            <CountriesList countries={this.state.countries}/>
+            <Switch>
+              <Route path='/:cca3' component={CountryDetails} />
+            </Switch>
           </div>
         </div>
       </div>
